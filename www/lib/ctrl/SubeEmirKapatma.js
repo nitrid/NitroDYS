ba599bc9773c959a7da255c719532317c0d08a09
recepkaraca1 <50820975+recepkaraca1@users.noreply.github.com>
2019-12-31 10:38:28 +0300

    ...

--- www/lib/ctrl/SubeEmirKapatma.js
+++ www/lib/ctrl/SubeEmirKapatma.js
@@ -1,7 +1,53 @@
 function SubeEmirKapatma ($scope,$window,db)
 {
 
-    
+    function TblSiparisSecimGrid(pData)
+    {                
+        $("#TblSipSecim").jsGrid
+        ({
+            width: "100%",
+            updateOnResize: true,
+            heading: true,
+            selecting: true,
+            data : pData,
+            paging : true,
+            pageSize: 5,
+            pageButtonCount: 3,
+            pagerFormat: "{pages} {next} {last}    {pageIndex} of {pageCount}",
+            fields: 
+            [
+                {
+                    name: "PALET",
+                    title: "PALET",
+                    type: "text",
+                    align: "center",
+                    width: 150
+                },
+                {
+                    name: "RAF",
+                    title: "RAF",
+                    type: "text",
+                    align: "center",
+                    width: 150
+                },
+                {
+                    name: "TIP",
+                    title: "TIP",
+                    type: "text",
+                    align: "center",
+                    width: 200
+                },
+                {
+                    name: "MIKTAR",
+                    title: "MIKTAR",
+                    type: "text",
+                    align: "center",
+                    width: 100
+                }
+            ],
+
+        });
+    }
     $scope.Init = function()
     {
         $scope.Firma = "NTGDB";
@@ -9,6 +55,10 @@ function SubeEmirKapatma ($scope,$window,db)
         UserParam = Param[$window.sessionStorage.getItem('User')];
         $scope.Seri = ''
         $scope.Tarih = moment(new Date()).format("DD.MM.YYYY");
+        $scope.SipTarih = moment(new Date()).format("DD.MM.YYYY");
+        $scope.SipTarih2 = moment(new Date()).format("DD.MM.YYYY");
+        $scope.MainClick();
+        TblSiparisSecimGrid()
         let TmpQuery = 
         {
             db : $scope.Firma,
@@ -16,9 +66,20 @@ function SubeEmirKapatma ($scope,$window,db)
         }
         db.GetDataQuery(TmpQuery,function(Data)
         {
-            TblSecimInit(Data);
-            $('#MdlSecim').modal('show');
+
         });
     }
+    $scope.Insert = function()
+    {
+        $("#TbBelgeBilgisi").addClass('active');
+        $("#TbMain").removeClass('active');
+    }
+    $scope.MainClick = function() 
+    {
+        $("#TbBelgeBilgisi").addClass('active');
+        $("#TbMain").removeClass('active');
+    }
+
+
 
 }

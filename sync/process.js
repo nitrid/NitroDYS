caf9d56367abb68eee67ea5b32e8e5a865416bb4
nitrid <alikemalkaraca@gmail.com>
2020-06-04 19:26:45 +0300

    .

--- sync/process.js
+++ sync/process.js
@@ -16,7 +16,7 @@ let Process =
             uid:"beka",
             pwd:"1122334455"
         },
-        auto: 6000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
+        auto: 300000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
         select :    //KAYITLARIN GETİRİLECEĞİ SELECT SORGUSU
         {
             query:"SELECT sto_kod AS KODU,sto_isim AS ADI,'' AS KATEGORI,dbo.fn_VergiYuzde(sto_toptan_vergi) AS KDV,sto_anagrup_kod AS ANAGRUPKODU FROM STOKLAR"
@@ -73,7 +73,7 @@ let Process =
             uid:"beka",
             pwd:"1122334455"
         },
-        auto: 8000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
+        auto: 300000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
         select :    //KAYITLARIN GETİRİLECEĞİ SELECT SORGUSU
         {
             query:"SELECT bar_kodu AS KODU,bar_stokkodu AS STOK,bar_birimpntr AS BIRIM FROM BARKOD_TANIMLARI"
@@ -126,7 +126,7 @@ let Process =
             uid:"beka",
             pwd:"1122334455"
         },
-        auto: 9000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
+        auto: 300000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
         select :    //KAYITLARIN GETİRİLECEĞİ SELECT SORGUSU
         {
             query:"SELECT sto_kod AS STOK,sto_birimID AS KODU,sto_birim_ad AS ADI,CASE WHEN sto_birim_katsayi < 0 THEN sto_birim_katsayi * -1 ELSE sto_birim_katsayi END AS KATSAYI FROM STOK_BIRIM_TANIMLARI_DIKEY"
@@ -183,7 +183,7 @@ let Process =
             uid:"beka",
             pwd:"1122334455"
         },
-        auto: 9000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
+        auto: 300000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
         select :    //KAYITLARIN GETİRİLECEĞİ SELECT SORGUSU
         {
             query:"SELECT " + 
@@ -197,7 +197,10 @@ let Process =
                   "CONVERT(nvarchar(10),sip_birim_pntr) AS BIRIM," + 
                   "sip_miktar AS MIKTAR," + 
                   "sip_teslim_miktar AS TESLIM_MIKTAR," + 
-                  "CONVERT(nvarchar(50),sip_Guid) AS OZEL " + 
+                  "CONVERT(nvarchar(50),sip_Guid) AS OZEL, " +
+                  "'' AS SATICI, " +
+                  "'' AS TASIYICI, " +
+                  "0 AS EMIRNO " + 
                   "FROM SIPARISLER WHERE sip_tip = 1 AND sip_cins = 0 AND sip_tarih >= GETDATE() - 10" 
         },
         control:    //KAYITLAR KARŞI TARAFADA VAR İSE GÜNCELLEMESİ İÇİN KONTROL SORGUSU
@@ -226,6 +229,9 @@ let Process =
                     ",[TESLIM_MIKTAR]" +
                     ",[OZEL]" +
                     ",[KAPALI]" +
+                    ",[SATICI]" +
+                    ",[TASIYICI]" +
+                    ",[EMIRNO]" +
                     ") VALUES ( " +
                     " 'Admin'						--<OKULLANICI, nvarchar(10),>\n" +
                     ",'Admin'						--<DKULLANICI, nvarchar(10),>\n" +
@@ -245,9 +251,13 @@ let Process =
                     ",@TESLIM_MIKTAR				--<TESLIM_MIKTAR, float,>\n" +
                     ",@OZEL							--<OZEL, nvarchar(50),>\n" +
                     ",0								--<KAPALI, smallint,>\n" +
+                    ",@SATICI						--<SATICI, nvarchar(15),>\n" +
+                    ",@TASIYICI						--<TASIYICI, nvarchar(15),>\n" +
+                    ",@EMIRNO						--<EMIRNO, int,>\n" +
                     ")",
             param : ['TARIH:string|10','SERI:string|10','SIRA:int','SATIRNO:int','KODU:string|25','GIRIS:string|25','CIKIS:string|25',
-                     'BIRIM:string|10','MIKTAR:float','TESLIM_MIKTAR:float','OZEL:string|50']
+                     'BIRIM:string|10','MIKTAR:float','TESLIM_MIKTAR:float','OZEL:string|50','SATICI:string|15','TASIYICI:string|15',
+                     'EMIRNO:int']
         }
     },
     {
@@ -266,7 +276,7 @@ let Process =
             uid:"beka",
             pwd:"1122334455"
         },
-        auto: 9000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
+        auto: 300000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
         select :    //KAYITLARIN GETİRİLECEĞİ SELECT SORGUSU
         {
             query:"SELECT " + 
@@ -280,7 +290,10 @@ let Process =
                   "CONVERT(nvarchar(10),sip_birim_pntr) AS BIRIM," + 
                   "sip_miktar AS MIKTAR," + 
                   "sip_teslim_miktar AS TESLIM_MIKTAR," + 
-                  "CONVERT(nvarchar(50),sip_Guid) AS OZEL " + 
+                  "CONVERT(nvarchar(50),sip_Guid) AS OZEL, " +
+                  "'' AS SATICI, " +
+                  "'' AS TASIYICI, " +
+                  "0 AS EMIRNO " +
                   "FROM SIPARISLER WHERE sip_tip = 0 AND sip_cins = 0 AND sip_tarih >= GETDATE() - 10" 
         },
         control:    //KAYITLAR KARŞI TARAFADA VAR İSE GÜNCELLEMESİ İÇİN KONTROL SORGUSU
@@ -309,6 +322,9 @@ let Process =
                     ",[TESLIM_MIKTAR]" +
                     ",[OZEL]" +
                     ",[KAPALI]" +
+                    ",[SATICI]" +
+                    ",[TASIYICI]" +
+                    ",[EMIRNO]" +
                     ") VALUES ( " +
                     " 'Admin'						--<OKULLANICI, nvarchar(10),>\n" +
                     ",'Admin'						--<DKULLANICI, nvarchar(10),>\n" +
@@ -328,10 +344,98 @@ let Process =
                     ",@TESLIM_MIKTAR				--<TESLIM_MIKTAR, float,>\n" +
                     ",@OZEL							--<OZEL, nvarchar(50),>\n" +
                     ",0								--<KAPALI, smallint,>\n" +
+                    ",@SATICI						--<SATICI, nvarchar(15),>\n" +
+                    ",@TASIYICI						--<TASIYICI, nvarchar(15),>\n" +
+                    ",@EMIRNO						--<EMIRNO, int,>\n" +
                     ")",
             param : ['TARIH:string|10','SERI:string|10','SIRA:int','SATIRNO:int','KODU:string|25','GIRIS:string|25','CIKIS:string|25',
-                     'BIRIM:string|10','MIKTAR:float','TESLIM_MIKTAR:float','OZEL:string|50']
+                     'BIRIM:string|10','MIKTAR:float','TESLIM_MIKTAR:float','OZEL:string|50','SATICI:string|15','TASIYICI:string|15',
+                     'EMIRNO:int']
         }
     }
+    // ,
+    // {
+    //     name: "ALIŞ İRSALİYESİ", //GÖRÜNEN ADI
+    //     source : //KAYNAK VERİTABANI BAĞLANTISI
+    //     {
+    //         server: "192.168.100.12",
+    //         database:"NTGDB",
+    //         uid:"beka",
+    //         pwd:"1122334455"
+    //     },
+    //     target : //HEDEF VERİTABANI BAĞLANTISI
+    //     {
+    //         server: "192.168.100.12",
+    //         database:"MikroDB_V16_TEST",
+    //         uid:"beka",
+    //         pwd:"1122334455"
+    //     },
+    //     //auto: 300000,  //OTOMATİK AKTARIM YAPILACAKSA BURAYA MİLİSANİYE CİNSİNDEN SÜRE YAZILIR (1000 = 1 SN) UNDEFINED VEYA BU KEY TANIMLANMAZSA OTOMATİK AKTARILMAZ.
+    //     select :    //KAYITLARIN GETİRİLECEĞİ SELECT SORGUSU
+    //     {
+    //         query:"SELECT " + 
+    //               "CONVERT(nvarchar(10),sip_tarih,112) AS TARIH," + 
+    //               "sip_evrakno_seri AS SERI," + 
+    //               "sip_evrakno_sira AS SIRA," + 
+    //               "sip_satirno AS SATIRNO," + 
+    //               "sip_stok_kod AS KODU," + 
+    //               "sip_musteri_kod AS GIRIS," + 
+    //               "CONVERT(nvarchar(25),sip_depono) AS CIKIS," + 
+    //               "CONVERT(nvarchar(10),sip_birim_pntr) AS BIRIM," + 
+    //               "sip_miktar AS MIKTAR," + 
+    //               "sip_teslim_miktar AS TESLIM_MIKTAR," + 
+    //               "CONVERT(nvarchar(50),sip_Guid) AS OZEL " + 
+    //               "FROM SIPARISLER WHERE sip_tip = 0 AND sip_cins = 0 AND sip_tarih >= GETDATE() - 10" 
+    //     },
+    //     control:    //KAYITLAR KARŞI TARAFADA VAR İSE GÜNCELLEMESİ İÇİN KONTROL SORGUSU
+    //     {
+    //         query: "SELECT OZEL FROM EMIRLER WHERE TIP = 1 AND CINS = 3 AND OZEL = @OZEL",
+    //         param:['OZEL:string|50']
+    //     },
+    //     insert:     //INSERT SORGUSU
+    //     {
+    //         query : "INSERT INTO [dbo].[EMIRLER](" +
+    //                 " [OKULLANICI]" +
+    //                 ",[DKULLANICI]" +
+    //                 ",[OTARIH]" +
+    //                 ",[DTARIH]" +
+    //                 ",[TIP]" +
+    //                 ",[CINS]" +
+    //                 ",[TARIH]" +
+    //                 ",[SERI]" +
+    //                 ",[SIRA]" +
+    //                 ",[SATIRNO]" +
+    //                 ",[KODU]" +
+    //                 ",[GIRIS]" +
+    //                 ",[CIKIS]" +
+    //                 ",[BIRIM]" +
+    //                 ",[MIKTAR]" +
+    //                 ",[TESLIM_MIKTAR]" +
+    //                 ",[OZEL]" +
+    //                 ",[KAPALI]" +
+    //                 ") VALUES ( " +
+    //                 " 'Admin'						--<OKULLANICI, nvarchar(10),>\n" +
+    //                 ",'Admin'						--<DKULLANICI, nvarchar(10),>\n" +
+    //                 ",GETDATE()						--<OTARIH, datetime,>\n" +
+    //                 ",GETDATE()						--<DTARIH, datetime,>\n" +
+    //                 ",1 							--<TIP, smallint,>\n" +
+    //                 ",3 							--<CINS, smallint,>\n" +
+    //                 ",@TARIH						--<TARIH, datetime,>\n" +
+    //                 ",@SERI							--<SERI, nvarchar(10),>\n" +
+    //                 ",@SIRA							--<SIRA, int,>\n" +
+    //                 ",@SATIRNO						--<SATIRNO, int,>\n" +
+    //                 ",@KODU							--<KODU, nvarchar(25),>\n" +
+    //                 ",@GIRIS						--<GIRIS, nvarchar(25),>\n" +
+    //                 ",@CIKIS						--<CIKIS, nvarchar(25),>\n" +
+    //                 ",@BIRIM						--<BIRIM, nvarchar(10),>\n" +
+    //                 ",@MIKTAR						--<MIKTAR, float,>\n" +
+    //                 ",@TESLIM_MIKTAR				--<TESLIM_MIKTAR, float,>\n" +
+    //                 ",@OZEL							--<OZEL, nvarchar(50),>\n" +
+    //                 ",0								--<KAPALI, smallint,>\n" +
+    //                 ")",
+    //         param : ['TARIH:string|10','SERI:string|10','SIRA:int','SATIRNO:int','KODU:string|25','GIRIS:string|25','CIKIS:string|25',
+    //                  'BIRIM:string|10','MIKTAR:float','TESLIM_MIKTAR:float','OZEL:string|50']
+    //     }
+    // }
 ]
 module.exports = Process;
